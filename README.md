418628e96834be84e840dc4e9d25b9435d992a6f
Gera <humbertova_mx@hotmail.com>
2019-07-10 13:39:23 -0500

    Firebase

--- README.md
+++ README.md
@@ -1,2 +1,100 @@
 # MiAppMensajeria
 Repositorio de mensajeria Firebase
+
+El primer paso es generar um muevo proyecto en Android studio en el cual vamos a implementar Firebase.
+
+![](.README_images/26a0f75c.png)
+
+El primer paso es obtener un añadir un proyecto nuevo en firebase, en la siguiente URL y añadir un nuevo proyecto.
+
+![](.README_images/d0e2a780.png)
+
+Una vez que cingresamos en la opción de genrar un nuevo proyecto, damos el nombre del proyecto a crear
+
+![](.README_images/0b57599d.png)
+
+De inmediato se te asigna un id de proyecto, el cual es muy importante, a continuaion se nos pide seleccionar una tecnología, seleccionamos Android
+
+![](.README_images/bd7d0267.png)
+
+En la siguiente ventana vamos a asignar el nombre del paquete de tu aplicación,el cual es el nombre del id de tu proyecto cuando lo creaste en android studio, lo puedes ver en la el archivo gradle de nivel de modulo o en el nombre del paquete que se generó en la main activity y que todas tus clases tendran. Es muy importante este paquete, ya que firebase va a enlazar tu app directamente a este nombr y si no coincide lo recharzará 
+
+![](.README_images/63018a56.png)
+
+Despues oprimes el boton de Registrar aplicación y el siguente paso es el archivo JSON de descarga de google.services, este archivo lo descargas y te vas a la vista Project de Android Studio y alli pegas ese archivo json dentro de la carpeta app.
+
+![](.README_images/e110eebd.png)
+
+![](.README_images/4d28272f.png)
+
+Despues debes de añadir el SDK de firebase a tu proyecto: Primero a nivel de gradle de proyecto vas a agregar
+
+![](.README_images/d74cd228.png)
+
+![](.README_images/e35859d6.png)
+
+Después en el gradle de nivel de módulo agregar la siguiente dependencia:
+
+![](.README_images/0cb56e1a.png)
+
+![](.README_images/48499191.png)
+
+Hasta abajo de ese archivo del build d nivel de modulo se agregará la siguiente linea:
+
+![](.README_images/c61da6a2.png)
+
+
+Debemos postetiormente de esto, generar las clases Kotlin:
+
+** MyFirebaseMessaginfService ** MyFirebaseInstanceIDService ** MyJobService 
+
+![](.README_images/d1228289.png)
+
+
+Del repositorio, ingresamos a las rutas para encontrar el código que llevará cada una de las clases que creamos anteriormente, el código que se tienen en éstas es el que indica la página oficial de google:
+
+![](.README_images/48f83b6b.png)
+
+Ingresamos a la clase MyFirebaseInstanceIDService y copiamos todo el código excepto el package:
+
+![](.README_images/a09100e6.png)
+
+Copiamos el siguiente código de MyFirebaseMessaginfService a nuestro proyecto creado:
+
+![](.README_images/9ba7af9b.png)
+
+Copiamos el siguiente código de MyJobService a nuestro proyecto creado:
+
+![](.README_images/ec82d98e.png)
+
+El siguiente paso es agregar los servicios de notificacion siguientes en el archivo AndroidManifest.xml, esto lo debes agregar inmediatamente abajo de la etiqueta de cierre de la activity del manifest
+como se inidca a continuación:
+
+![](.README_images/3e2a1963.png)
+
+![](.README_images/7dcb1b6c.png)
+
+```
+<!-- Agregamos aqui la parte de el servicio de mensajeria -->
+        <service android:name=".MyFirebaseMessagingService">
+            <intent-filter>
+                <action android:name="com.google.firebase.MESSAGING_EVENT"/>
+            </intent-filter>
+        </service>
+        <service android:name=".MyFirebaseInstanceIDService">
+            <intent-filter>
+                <action android:name="com.google.firebase.INSTANCE_ID_EVENT"/>
+            </intent-filter>
+        </service>
+        <service
+            android:name=".MyJobService"
+            android:exported="false">
+            <intent-filter>
+                <action android:name="com.firebase.jobdispatcher.ACTION_EXECUTE"/>
+            </intent-filter>
+        </service>
+```
+
+
+
+
--- app/src/main/java/org/mensajeria/cotizacion/MyFirebaseMessaginfService.kt
+++ app/src/main/java/org/mensajeria/cotizacion/MyFirebaseMessaginfService.kt
@@ -1,2 +1,148 @@
 package org.mensajeria.cotizacion
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.media.RingtoneManager
+import android.os.Build
+
+import android.util.Log
+
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import android.media.AudioAttributes
+import androidx.core.app.NotificationCompat
+
+import com.firebase.jobdispatcher.FirebaseJobDispatcher
+import com.firebase.jobdispatcher.GooglePlayDriver
+
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    /**
+     * Called when message is received.
+     *
+     * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
+     */
+    // [START receive_message]
+    override fun onMessageReceived(remoteMessage: RemoteMessage?) {
+        // [START_EXCLUDE]
+        // There are two types of messages data messages and notification messages. Data messages are handled
+        // here in onMessageReceived whether the app is in the foreground or background. Data messages are the type
+        // traditionally used with GCM. Notification messages are only received here in onMessageReceived when the app
+        // is in the foreground. When the app is in the background an automatically generated notification is displayed.
+        // When the user taps on the notification they are returned to the app. Messages containing both notification
+        // and data payloads are treated as notification messages. The Firebase console always sends notification
+        // messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options
+        // [END_EXCLUDE]
+
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: " + remoteMessage!!.from!!)
+
+        // Check if message contains a data payload.
+        if (remoteMessage.data.size > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.data)
+
+            if (/* Check if data needs to be processed by long running job */ true) {
+                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
+                scheduleJob()
+            } else {
+                // Handle message within 10 seconds
+                handleNow()
+            }
+
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.notification != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.notification!!.body!!)
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+
+        sendNotification("Ecatepec 33868:"+remoteMessage.getData().get("body"));
+    }
+    // [END receive_message]
+
+    /**
+     * Schedule a job using FirebaseJobDispatcher.
+     */
+    private fun scheduleJob() {
+        // [START dispatch_job]
+        val dispatcher = FirebaseJobDispatcher(GooglePlayDriver(this))
+        val myJob = dispatcher.newJobBuilder()
+            .setService(MyJobService::class.java)
+            .setTag("my-job-tag")
+            .build()
+        dispatcher.schedule(myJob)
+        // [END dispatch_job]
+    }
+
+    /**
+     * Handle time allotted to BroadcastReceivers.
+     */
+    private fun handleNow() {
+        Log.d(TAG, "Short lived task is done.")
+    }
+
+    /**
+     * Create and show a simple notification containing the received FCM message.
+     *
+     * @param messageBody FCM message body received.
+     */
+    private fun sendNotification(messageBody: String) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+            PendingIntent.FLAG_ONE_SHOT)
+
+
+        val channelId = getString(R.string.default_notification_channel_id)
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+
+        // val sonido = Uri.parse("android.resource://sonido/alerta.mp3")
+        val attributes = AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+            .build()
+
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_stat_ic_notification)
+            .setContentTitle("Mensaje nuevo ")
+
+            .setContentText(messageBody)
+
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)  .setPriority(NotificationCompat.PRIORITY_HIGH).setAutoCancel(true)
+
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(channelId,
+                "Canal de salud",
+                NotificationManager.IMPORTANCE_HIGH)
+            channel.enableLights(true)
+            channel.lightColor=Color.RED
+            channel.setSound(defaultSoundUri,attributes)
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+
+
+
+        }
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
+    }
+
+    companion object {
+
+        private val TAG = "MyFirebaseMsgService"
+    }
+}
