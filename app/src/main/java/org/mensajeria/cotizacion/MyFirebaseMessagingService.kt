29a5258f9efc99f26a08417d18bd7d0f79f3225a
campitos <rapidclimate@gmail.com>
2019-07-26 08:57:39 -0500

    Ya esta con la imagen de la izquierda gerardito

--- app/src/main/java/org/mensajeria/cotizacion/MyFirebaseMessagingService.kt
+++ app/src/main/java/org/mensajeria/cotizacion/MyFirebaseMessagingService.kt
@@ -1,32 +1,20 @@
 package org.mensajeria.cotizacion
 
-import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.graphics.Color
+import android.graphics.BitmapFactory
 import android.media.RingtoneManager
 import android.os.Build
-
+import androidx.core.app.NotificationCompat
 import android.util.Log
-
-
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
-import android.media.AudioAttributes
-import androidx.core.app.NotificationCompat
-
-import com.firebase.jobdispatcher.FirebaseJobDispatcher
-import com.firebase.jobdispatcher.GooglePlayDriver
-import android.R
-import android.graphics.BitmapFactory
-
-
-
-
-
+import org.mensajeria.cotizacion.MainActivity
 
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
@@ -50,45 +38,55 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
         // TODO(developer): Handle FCM messages here.
         // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
-        Log.i(TAG, "de" + remoteMessage!!.from!!)
+        Log.d(TAG, "From: ${remoteMessage?.from}")
 
         // Check if message contains a data payload.
-        if (remoteMessage.data.size > 0) {
-            Log.i(TAG, "Message data payload: " + remoteMessage.data)
+        remoteMessage?.data?.isNotEmpty()?.let {
+            Log.d(TAG, "Message data payload: " + remoteMessage.data)
 
             if (/* Check if data needs to be processed by long running job */ true) {
-                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
+                // For long-running tasks (10 seconds or more) use WorkManager.
                 scheduleJob()
             } else {
                 // Handle message within 10 seconds
                 handleNow()
             }
-
         }
 
         // Check if message contains a notification payload.
-        if (remoteMessage.notification != null) {
-            Log.d(TAG, "Message Notification Body: " + remoteMessage.notification!!.body!!)
+        remoteMessage?.notification?.let {
+            Log.d(TAG, "Message Notification Body: ${it.body}")
         }
+        sendNotification(remoteMessage?.data?.get("title").toString()+" "+remoteMessage?.getData()?.get("body"))
 
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
-
-        sendNotification(remoteMessage.data.get("title").toString()+" "+remoteMessage.getData().get("body"));
     }
     // [END receive_message]
 
+    // [START on_new_token]
     /**
-     * Schedule a job using FirebaseJobDispatcher.
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the InstanceID token
+     * is initially generated so this is where you would retrieve the token.
+     */
+    override fun onNewToken(token: String?) {
+        Log.d(TAG, "Refreshed token: $token")
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(token)
+    }
+    // [END on_new_token]
+
+    /**
+     * Schedule async work using WorkManager.
      */
     private fun scheduleJob() {
         // [START dispatch_job]
-        val dispatcher = FirebaseJobDispatcher(GooglePlayDriver(this))
-        val myJob = dispatcher.newJobBuilder()
-            .setService(MyJobService::class.java)
-            .setTag("my-job-tag")
-            .build()
-        dispatcher.schedule(myJob)
+        val work = OneTimeWorkRequest.Builder(MyWorker::class.java).build()
+        WorkManager.getInstance().beginWith(work).enqueue()
         // [END dispatch_job]
     }
 
@@ -100,59 +98,57 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     }
 
     /**
+     * Persist token to third-party servers.
+     *
+     * Modify this method to associate the user's FCM InstanceID token with any server-side account
+     * maintained by your application.
+     *
+     * @param token The new token.
+     */
+    private fun sendRegistrationToServer(token: String?) {
+        // TODO: Implement this method to send token to your app server.
+    }
+
+    /**
      * Create and show a simple notification containing the received FCM message.
      *
      * @param messageBody FCM message body received.
      */
-    @SuppressLint("NewApi")
     private fun sendNotification(messageBody: String) {
         val intent = Intent(this, MainActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
         val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
             PendingIntent.FLAG_ONE_SHOT)
 
-
-        val channelId = "Canal malo"
+        val channelId = getString(R.string.default_notification_channel_id)
         val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+  val color=resources.getColor(R.color.colorAccent)
 
-        // val sonido = Uri.parse("android.resource://sonido/alerta.mp3")
-        val attributes = AudioAttributes.Builder()
-            .setUsage(AudioAttributes.USAGE_NOTIFICATION)
-            .build()
-        val color = resources.getColor(R.color.holo_orange_dark)
-        val largeIcon = BitmapFactory.decodeResource(resources,R.drawable.ic_dialog_email)
+        val larga= BitmapFactory.decodeResource(resources, R.drawable.body)
 
 
         val notificationBuilder = NotificationCompat.Builder(this, channelId)
-            .setSmallIcon(R.drawable.ic_dialog_map)
+            .setSmallIcon(R.drawable.ic_loyaltito)
             .setColor(color)
             .setColorized(true)
-
-
-            .setLargeIcon(largeIcon)
-            .setContentTitle("Mensaje nuevo ")
-
+            .setLargeIcon(larga)
+            .setStyle(NotificationCompat.BigPictureStyle()
+                .bigPicture(larga)
+                .bigLargeIcon(null))
             .setContentText(messageBody)
 
+            .setAutoCancel(true)
             .setSound(defaultSoundUri)
-            .setContentIntent(pendingIntent) .setPriority(NotificationCompat.PRIORITY_HIGH).setAutoCancel(true)
-
+            .setContentIntent(pendingIntent)
 
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-        // A partir de oreo(Android ) las notificaciones se agregan con esto.
+        // Since android Oreo notification channel is needed.
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(channelId,
-                "Mensajeria gera",
-                NotificationManager.IMPORTANCE_HIGH)
-            channel.enableLights(true)
-            channel.lightColor=Color.RED
-            channel.setSound(defaultSoundUri,attributes)
-            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                "Channel human readable title",
+                NotificationManager.IMPORTANCE_DEFAULT)
             notificationManager.createNotificationChannel(channel)
-
-
-
         }
 
         notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
@@ -160,6 +156,6 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     companion object {
 
-        private val TAG = "puto"
+        private const val TAG = "puto"
     }
 }
